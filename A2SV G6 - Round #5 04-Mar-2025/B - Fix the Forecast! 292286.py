3958182ac735bb1aec447464ca97dc3fd6719e11
Tsion_Mel <tsionmelese526@gmail.com>
2025-03-04 22:44:13 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 04-Mar-2025/B - Fix the Forecast! 292286.py
@@ -0,0 +1,23 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+from collections import defaultdict
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    sorted_a = sorted(a)
+    sorted_b = sorted(b)
+    dicc = defaultdict(list)
+
+    for i in range(n):
+        dicc[sorted_a[i]].append(sorted_b[i])
+
+    output = []
+    for i in range(n):
+        output.append(dicc[a[i]].pop())
+
+    for i in range(n):
+        print(output[i], end=" ")
+    print()
