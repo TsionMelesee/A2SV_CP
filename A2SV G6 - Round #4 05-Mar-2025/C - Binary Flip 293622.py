ba7b8b10c31c99ffbbbf99acfd5c862898705d30
Tsion_Mel <tsionmelese526@gmail.com>
2025-03-05 23:01:49 +0300

    Created 'C - Binary Flip' by A2SV Hub

---
+++ A2SV G6 - Round #4 05-Mar-2025/C - Binary Flip 293622.py
@@ -0,0 +1,28 @@
+# Problem: C - Binary Flip - https://codeforces.com/gym/590053/problem/C
+
+x = int(input())
+for _ in range(x):
+    n = int(input())
+    a = list(input())
+    b = list(input())
+    
+    prefix = [int(a[0])]
+    for i in range(1, n):
+        prefix.append(prefix[-1] + int(a[i]))
+    
+    flips = 0
+    flag = True
+    
+    for i in range(n - 1, -1, -1):
+        if flips % 2 == 0 and a[i] != b[i]:
+            if (i + 1) != prefix[i] * 2:
+                flag = False
+                break
+            flips += 1
+        elif flips % 2 != 0 and a[i] == b[i]:
+            if (i + 1) != prefix[i] * 2:
+                flag = False
+                break
+            flips += 1
+    
+    print("YES" if flag else "NO")
