3ad7634865b10c8b4c53af08ef3d3e9d3b4702fa
Tsion_Mel <tsionmelese526@gmail.com>
2025-03-12 19:55:19 +0300

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/E - Minimum Subsequence 304242.py
@@ -0,0 +1,46 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+from collections import defaultdict
+
+
+x =int(input())
+for _ in range(x):
+    a = int(input())
+    arr = ((input()))
+    one = []
+    zero =[]
+    ans = []
+    temp =1
+    if arr[0]=='1':
+        one.append(1)
+    else:
+        zero.append(1)
+    ans.append(1)
+    for i in range(1,len(arr)):
+        if arr[i]=='0':
+            if len(one)>0:
+                popped = one.pop()
+                ans.append(popped)
+                
+                zero.append(popped)
+            else:
+                temp+=1
+                ans.append(temp)
+                zero.append(temp)
+        elif arr[i]=='1':
+            if len(zero)>0:
+                popped = zero.pop()
+                ans.append(popped)
+                
+                one.append(popped)
+            else:
+                temp+=1
+                ans.append(temp)
+                one.append(temp)
+    print(temp)
+    print(*ans)
+
+
+
+
+        
