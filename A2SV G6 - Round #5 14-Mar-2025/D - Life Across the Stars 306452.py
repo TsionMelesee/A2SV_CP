24829203ed8ef508a632dce43f3f26d29ab9b399
Tsion_Mel <tsionmelese526@gmail.com>
2025-03-14 14:25:37 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 14-Mar-2025/D - Life Across the Stars 306452.py
@@ -0,0 +1,22 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import Counter
+x = int(input())
+prefix = Counter()
+
+for _ in range(x):
+    l, r = map(int, input().split())
+    prefix[l] += 1
+    prefix[r] -= 1
+
+prefix =  list(prefix.items())
+prefix.sort()
+val,max_val,year = 0,0,0
+
+for i in range(len(prefix)):
+    val += prefix[i][1]
+    if val > max_val:
+        max_val = val
+        year = prefix[i][0]
+
+print(year, max_val)
