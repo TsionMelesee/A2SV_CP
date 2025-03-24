685cf8fa0ac94916dd7db69909448f5f38c2756b
Tsion_Mel <tsionmelese526@gmail.com>
2025-03-24 20:59:25 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 24-Mar-2025/Red and Blue 317149.py
@@ -0,0 +1,20 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+for _ in range(int(input())): 
+    x = int(input()) 
+    red = list(map(int, input().split()))
+    y = int(input()) 
+    blue = list(map(int, input().split()))
+    red_prefix = 0
+    red_max = 0
+    for i in range(x):
+        red_prefix += red[i]
+        red_max = max(red_max, red_prefix)
+
+    blue_prefix = 0
+    blue_max = 0
+    for i in range(y):
+        blue_prefix += blue[i]
+        blue_max = max(blue_max, blue_prefix)
+
+    print(max(0, red_max + blue_max))
